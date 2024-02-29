c0677ae3c2a6196e6e1e90f78176cb40442a75cb
DominicTheDestroyer <161382730+DominicTheDestroyer@users.noreply.github.com>
2024-02-29 15:10:55 +1300

    Update GeoDash.js

--- GeoDash.js
+++ GeoDash.js
@@ -10,14 +10,29 @@ const SCREEN_HEIGHT = 200;
 const PLAYER_HEIGHT = 25;
 const PLAYER_WIDTH = 25;
 
+
+const OBSTACLE_HEIGHT = PLAYER_HEIGHT;
+const OBSTACLE_WIDTH = PLAYER_WIDTH;
+
+
+var obstacles;
 /*******************************************************/
 // setup()
 /*******************************************************/
 function setup() {
     console.log("setup: ");
     cnv= new Canvas(SCREEN_WIDTH, SCREEN_HEIGHT);
+    
     player = new Sprite(PLAYER_WIDTH*1.2,  SCREEN_HEIGHT/2, PLAYER_WIDTH, PLAYER_HEIGHT, 'd');
     player.color = color("purple");
+    
+    obstacle = new Sprite(SCREEN_WIDTH-100,  SCREEN_HEIGHT - OBSTACLE_HEIGHT/2, OBSTACLE_WIDTH, OBSTACLE_HEIGHT, 'k');
+    obstacle.color = color("blue");
+    obstacle.vel.x = -10;
+    
+    obstacles = new Group();
+    obstacles.add(obstacle)
+    
     floor =  new Sprite(SCREEN_WIDTH/2,  SCREEN_HEIGHT, SCREEN_WIDTH, 4, 's');
     floor.color = color("black");
     world.gravity.y = 80;
@@ -31,17 +46,27 @@ function setup() {
                 player.vel.y = -20;
             }
     });
+    
+     player.collides(obstacles, youDead);
 }
+
 
 /*******************************************************/
 // draw()
 /*******************************************************/
 function draw() {
   background('lightblue');
-  
-  
+  newObstacle();
+}
+function newObstacle(){
+    obstacle = new Sprite(SCREEN_WIDTH-100,  SCREEN_HEIGHT - OBSTACLE_HEIGHT/2, OBSTACLE_WIDTH, OBSTACLE_HEIGHT, 'k');
+    obstacle.color = color("blue");
+    obstacle.vel.x = -10;
 }
 
+function youDead(_player, _obstacle){
+    console.log("YouDied")
+}
 /*******************************************************/
 //  END OF APP
 /*******************************************************/
